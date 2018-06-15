4a48202b666aabc6046f91b584b00933ee404d9c
Jkocevska <j.kocevska98@gmail.com>
2018-06-15 12:06:52 +0200

    Add files via upload

---
+++ StartPage/StartPage/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StartPage
+{
+    public partial class Form1 : Form
+    {
+        JumpAnswerSurvive inJumpAnswerSurvive = new JumpAnswerSurvive();
+        Rules inRules = new Rules();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+            inJumpAnswerSurvive.Show();
+        }
+
+        private void ShowRules(object sender, EventArgs e)
+        {
+            inRules.Show();
+        }
+    }
+}
